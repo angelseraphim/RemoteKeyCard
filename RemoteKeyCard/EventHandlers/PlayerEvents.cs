3e9996c5e20bb103109a8721d64fedcf763ce61a
Angel seraphim <server@praniksl.ru>
2025-04-26 18:01:50 +0500

    Теперь код выглядит нормально

---
+++ RemoteKeyCard/EventHandlers/PlayerEvents.cs
@@ -0,0 +1,65 @@
+﻿using Exiled.Events.EventArgs.Player;
+using Interactables.Interobjects.DoorUtils;
+using MapGeneration.Distributors;
+using System.Reflection;
+
+namespace RemoteKeyCard.EventHandlers
+{
+    internal class PlayerEvents
+    {
+        internal void Register()
+        {
+            Exiled.Events.Handlers.Player.InteractingDoor += OnInteractingDoor;
+            Exiled.Events.Handlers.Player.UnlockingGenerator += OnUnlockingGenerator;
+            Exiled.Events.Handlers.Player.ActivatingWarheadPanel += OnActivatingWarheadPanel;
+            Exiled.Events.Handlers.Player.InteractingLocker += OnInteractingLocker;
+        }
+
+        internal void Unregister()
+        {
+            Exiled.Events.Handlers.Player.InteractingDoor -= OnInteractingDoor;
+            Exiled.Events.Handlers.Player.UnlockingGenerator -= OnUnlockingGenerator;
+            Exiled.Events.Handlers.Player.ActivatingWarheadPanel -= OnActivatingWarheadPanel;
+            Exiled.Events.Handlers.Player.InteractingLocker -= OnInteractingLocker;
+        }
+
+        private void OnInteractingDoor(InteractingDoorEventArgs ev)
+        {
+            if (ev.Player == null || ev.Door.IsLocked || ev.IsAllowed)
+                return;
+
+            if (Plugin.CheckPermission(ev.Player, ev.Door.RequiredPermissions.RequiredPermissions))
+                ev.IsAllowed = true;
+        }
+
+        private void OnUnlockingGenerator(UnlockingGeneratorEventArgs ev)
+        {
+            if (ev.Player == null || ev.IsAllowed)
+                return;
+            
+            var requiredPermissionField = typeof(Scp079Generator).GetField(nameof(Scp079Generator._requiredPermission), BindingFlags.NonPublic | BindingFlags.Instance);
+            var requiredPermission = (KeycardPermissions)requiredPermissionField.GetValue(ev.Generator.Base);
+
+            if (Plugin.CheckPermission(ev.Player, requiredPermission))
+                ev.IsAllowed = true;
+        }
+
+        private void OnActivatingWarheadPanel(ActivatingWarheadPanelEventArgs ev)
+        {
+            if (ev.Player == null || ev.IsAllowed)
+                return;
+
+            if (Plugin.CheckPermission(ev.Player, KeycardPermissions.AlphaWarhead))
+                ev.IsAllowed = true;
+        }
+
+        private void OnInteractingLocker(InteractingLockerEventArgs ev)
+        {
+            if (ev.Player == null || ev.IsAllowed)
+                return;
+
+            if (Plugin.CheckPermission(ev.Player, ev.InteractingChamber.Base.RequiredPermissions))
+                ev.IsAllowed = true;
+        }
+    }
+}
--- RemoteKeyCard/Plugin.cs
+++ RemoteKeyCard/Plugin.cs
@@ -1,10 +1,7 @@
-﻿using System.Linq;
-using Exiled.API.Features;
-using Exiled.Events.EventArgs.Player;
+﻿using Exiled.API.Features;
 using Exiled.API.Features.Items;
+using RemoteKeyCard.EventHandlers;
 using KeycardPermission = Interactables.Interobjects.DoorUtils.KeycardPermissions;
-using MapGeneration.Distributors;
-using System.Reflection;
 
 namespace RemoteKeyCard
 {
@@ -13,70 +10,37 @@ namespace RemoteKeyCard
         public override string Prefix => "RemoteKeyCard";
         public override string Name => "RemoteKeyCard";
         public override string Author => "angelseraphim.";
+
+        private PlayerEvents playerEvents;
+
         public override void OnEnabled()
         {
-            Exiled.Events.Handlers.Player.InteractingDoor += OnInteractingDoor;
-            Exiled.Events.Handlers.Player.UnlockingGenerator += OnUnlockingGenerator;
-            Exiled.Events.Handlers.Player.ActivatingWarheadPanel += OnActivatingWarheadPanel;
-            Exiled.Events.Handlers.Player.InteractingLocker += OnInteractingLocker;
+            playerEvents = new PlayerEvents();
+            playerEvents.Register();
+
             base.OnEnabled();
         }
 
         public override void OnDisabled()
         {
-            Exiled.Events.Handlers.Player.InteractingDoor -= OnInteractingDoor;
-            Exiled.Events.Handlers.Player.UnlockingGenerator -= OnUnlockingGenerator;
-            Exiled.Events.Handlers.Player.ActivatingWarheadPanel -= OnActivatingWarheadPanel;
-            Exiled.Events.Handlers.Player.InteractingLocker -= OnInteractingLocker;
+            playerEvents.Unregister();
+            playerEvents = null;
+
             base.OnDisabled();
         }
-        private bool CheckPermission(Player player, KeycardPermission keycardPermissions)
+
+        internal static bool CheckPermission(Player player, KeycardPermission keycardPermissions)
         {
-            foreach (Item item in player.Items.Where(i => i.IsKeycard))
+            foreach (Item item in player.Items)
             {
                 if (item is Keycard keycard)
                 {
-                    Log.Debug(keycard.Base.Permissions + "\n" + keycardPermissions);
                     if ((keycard.Base.Permissions & keycardPermissions) != 0)
                         return true;
                 }
             }
+
             return false;
-        }
-        private void OnInteractingDoor(InteractingDoorEventArgs ev)
-        {
-            if (ev.Player == null || ev.Door.IsLocked || ev.IsAllowed)
-                return;
-
-            if (CheckPermission(ev.Player, ev.Door.RequiredPermissions.RequiredPermissions))
-                ev.IsAllowed = true;
-        }
-        private void OnUnlockingGenerator(UnlockingGeneratorEventArgs ev)
-        {
-            if (ev.Player == null || ev.IsAllowed)
-                return;
-
-            var requiredPermissionField = typeof(Scp079Generator).GetField(nameof(Scp079Generator._requiredPermission), BindingFlags.NonPublic | BindingFlags.Instance);
-            var requiredPermission = (KeycardPermission)requiredPermissionField.GetValue(ev.Generator.Base);
-
-            if (CheckPermission(ev.Player, requiredPermission))
-                ev.IsAllowed = true;
-        }
-        private void OnActivatingWarheadPanel(ActivatingWarheadPanelEventArgs ev)
-        {
-            if (ev.Player == null || ev.IsAllowed)
-                return;
-
-            if (CheckPermission(ev.Player, KeycardPermission.AlphaWarhead))
-                ev.IsAllowed = true;
-        }
-        private void OnInteractingLocker(InteractingLockerEventArgs ev)
-        {
-            if (ev.Player == null || ev.IsAllowed)
-                return;
-
-            if (CheckPermission(ev.Player, ev.Chamber.RequiredPermissions))
-                ev.IsAllowed = true;
         }
     }
 }
